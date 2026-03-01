61bcb4ea7b3a59b8c19194f5ce17e9c2d0a49cae
rTexty <kamil.axmadiev88@gmail.com>
2026-03-01 19:44:11 +0300

    folder algo with implementations of algorithms

---
+++ algo/quicksort.go
@@ -0,0 +1,23 @@
+package algo
+
+
+
+func Find(sl []int, target int) int {
+	low := 0
+	high := len(sl)
+	for low <= high{
+		mid := low + (high- low )/ 2
+		guess := sl[mid]
+		if guess == target{
+			return mid
+		}
+		if guess > target {
+			high = mid -1
+		} else{
+			low = mid +1
+		}
+	}
+	return -1
+	
+}
+
